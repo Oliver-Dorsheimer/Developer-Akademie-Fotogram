40b5d19f706fb80203c96242cd0f91b7bc85fd33
Oliver Dorsheimer <developer-akademie.nifty066@8alias.com>
2026-03-03 18:15:13 +0100

    worked on image generation, throught java script

--- script.js
+++ script.js
@@ -23,19 +23,84 @@ let image_array = [
 ##############################################################                                Main                                           ######################################################################################
 #################################################################################################################################################################################################################################*/
 
-
-function create_images(){
-    for (let i = 0; i < image_array.length; i++){
-        document.getElementById(foto_container).innerHTML();
-    };
-
-};
-
-
-
-
+for (let i = 0; i < image_array.length; i++){
+    console.count(image_array[i]);
+}
 
 
 /*#################################################################################################################################################################################################################################
 ##############################################################                                Functions                                      ######################################################################################
 #################################################################################################################################################################################################################################*/
+
+
+function render_images(){
+    let container = document.getElementById("foto_container");
+    for (let i = 0; i < image_array.length; i++){
+        container.innerHTML += create_image(i,image_array[i]);
+
+    }
+
+    yeah();
+
+};
+
+function yeah(){
+    for (let i = 0; i < image_array.length; i++){
+        document.getElementById(image_array[i]).hasAttribute("height");
+        document.getElementById(image_array[i]).hasAttribute("width");
+        document.getElementById(image_array[i]).hasAttribute
+    }
+}
+
+function create_image(index, id_name) {
+    return "<img class = " + id_name + "></img>";
+};
+
+function create_image_structure (index){
+    let img = document.createElement('img');
+    img.setAttribute = "src", `${image_array[i]}`;
+    img.setAttribute("width", "304");
+    img.setAttribute("height", "228");
+    img.setAttribute("alt", "The Pulpit Rock");
+    return img;
+};
+
+function myFunction(i) {
+  var x = document.createElement("IMG");
+  x.setAttribute("src", `${image_array[i]}`);
+  x.setAttribute("width", "304");
+  x.setAttribute("height", "228");
+  x.setAttribute("alt", "The Pulpit Rock");
+  document.body.appendChild(x);
+  return x
+
+}
+
+
+/*###########################################
+#                Stille Ecke                #
+#############################################
+
+        <img src = "`${image_array[index]}`"></img>
+        background-image: url(`${image_array[index]}`);
+
+
+        for (let i = 0; i < image_array.length; i++){
+        document.getElementById(foto_container).innerHTML.create_image_structure += myFunction(i);
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
